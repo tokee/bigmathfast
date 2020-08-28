78ca2dd215d293823e95e13734696f63ca09a7b0
Toke Eskildsen <te@ekot.dk>
2020-08-28 19:20:58 +0200

    Bugfix: Ecm, not ecm

--- src/test/java/dk/thomasegense/bigmathfast/primes/Test.java
+++ src/test/java/dk/thomasegense/bigmathfast/primes/Test.java
@@ -9,7 +9,7 @@ public class Test {
     public static void main(String[] args) {
        
         
-  System.out.println(ecm.factor(new BigInteger("30323232323232323231243244272")));
+  System.out.println(Ecm.factor(new BigInteger("30323232323232323231243244272")));
         
         
         
